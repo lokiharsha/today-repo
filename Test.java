88f42e6c600dcbcf6aa4a35d56f4f14d2e8056c8
lokiharsha <lakshmipathi.devops76@gmail.com>
2024-08-16 07:48:16 +0530

    for Test.java ; is added

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
   public static void main(String args[]){
     System.out.println("welcome to devops");
-    System.out.println("welcome to AWS")
+    System.out.println("welcome to AWS");
     
   }
 }
